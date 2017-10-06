6e591fddbc9f8ca6d57b12f0fa6fc55ed5d5a65e
Hari <hariprasath.periyasamy@gmail.com>
2017-10-06 18:16:33 -0400

    modified callbackURL
    http://localhost:5000/auth/google/callback

--- server/index.js
+++ server/index.js
@@ -5,24 +5,33 @@ const keys = require('./config/keys');
 
 const app = express(); //create express app
 
-
 passport.use(
   new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL:'auth/google/callback'
-  }, (accessToken)=>{
+    callbackURL:'http://localhost:5000/auth/google/callback'
+  },
+  accessToken => {
     console.log(accessToken);
-  })
+  }
+)
 );
 
+//first time authentication
 app.get(
   '/auth/google',
 passport.authenticate('google',{
   scope:['profile','email']
 }))
 
-app.get('auth/google/callback',passport.authenticate('google'));
+//already registered and contains the code
+app.get('/auth/google/callback',passport.authenticate('google'));//it will have the code available
 
 const PORT = (process.env.PORT || 5000); //process.env.PORT is for Heroku and 5000 is local dev port
 app.listen(PORT);
+
+
+//https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Fgoogle%2Fcallback&scope=profile%20email&client_id=894213990449-7kq1cgctk936qsi58q3jpqt1dtm9mbis.apps.googleusercontent.com
+//http://localhost:5000/auth/google/callback?code=4/TKpLBHQ_UtOCx73LRFXhAMEDLTChOEZ923RYmoYnFA8#
+
+//https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Fgoogle%2Fcallback&client_id=894213990449-7kq1cgctk936qsi58q3jpqt1dtm9mbis.apps.googleusercontent.com
