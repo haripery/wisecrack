1297379c83f0e66d3c1efa545568b61882ebd952
Hari <hariprasath.periyasamy@gmail.com>
2017-10-05 21:24:40 -0400

    authenticate passport

--- server/index.js
+++ server/index.js
@@ -1,9 +1,28 @@
-const express = require('express');
-const app = express();
+const express = require('express'); //import express folder
+const passport = require('passport'); //passport for authentication
+const GoogleStrategy = require('passport-google-oauth20').Strategy; //passport-google-oauth20 strategy import
+const keys = require('./config/keys');
 
-app.get('/',(req, res) => {
-  res.send({hi:'there'});
-});
+const app = express(); //create express app
 
-const PORT = (process.env.PORT || 5000);
+
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL:'auth/google/callback'
+  }, (accessToken)=>{
+    console.log(accessToken);
+  })
+);
+
+app.get(
+  '/auth/google',
+passport.authenticate('google',{
+  scope:['profile','email']
+}))
+
+app.get('auth/google/callback',passport.authenticate('google'));
+
+const PORT = (process.env.PORT || 5000); //process.env.PORT is for Heroku and 5000 is local dev port
 app.listen(PORT);
