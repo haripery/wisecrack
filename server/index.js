65c159dcedb23cef6fac9c3bc70eb9145c143e09
Hari <hariprasath.periyasamy@gmail.com>
2017-10-08 19:54:37 -0400

    user instance created and saved in database

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express'); //import express folder
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
+
 
 mongoose.connect(keys.mongoURI);
 
@@ -11,10 +13,3 @@ require('./routes/authRoutes')(app);
 
 const PORT = (process.env.PORT || 5000); //process.env.PORT is for Heroku and 5000 is local dev port
 app.listen(PORT);
-
-
-
-//https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Fgoogle%2Fcallback&scope=profile%20email&client_id=894213990449-7kq1cgctk936qsi58q3jpqt1dtm9mbis.apps.googleusercontent.com
-//http://localhost:5000/auth/google/callback?code=4/TKpLBHQ_UtOCx73LRFXhAMEDLTChOEZ923RYmoYnFA8#
-
-//https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Fgoogle%2Fcallback&client_id=894213990449-7kq1cgctk936qsi58q3jpqt1dtm9mbis.apps.googleusercontent.com
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport'); //passport for authentication
 const GoogleStrategy = require('passport-google-oauth20').Strategy; //passport-google-oauth20 strategy import
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy({
@@ -9,9 +12,12 @@ passport.use(
     callbackURL:'http://localhost:5000/auth/google/callback'
   },
   (accessToken, refreshToken, profile, done) => {
-    console.log('access Token',accessToken);
-    console.log('Refresh Token',refreshToken);
-    console.log('profile',profile);
+    new User({googleId:profile.Id}).save();
   }
 )
 );
+
+//https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Fgoogle%2Fcallback&scope=profile%20email&client_id=894213990449-7kq1cgctk936qsi58q3jpqt1dtm9mbis.apps.googleusercontent.com
+//http://localhost:5000/auth/google/callback?code=4/TKpLBHQ_UtOCx73LRFXhAMEDLTChOEZ923RYmoYnFA8#
+
+//https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Fgoogle%2Fcallback&client_id=894213990449-7kq1cgctk936qsi58q3jpqt1dtm9mbis.apps.googleusercontent.com
