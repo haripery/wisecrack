b313b9c0b43a57ac6dde391d63561ffb651db30a
Hari <hariprasath.periyasamy@gmail.com>
2017-10-30 21:38:58 -0400

    react router with components

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './Header';
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
 
 const App = () => {
   return (
     <div>
-      Hi There !
+      <BrowserRouter>
+      <div>
+        <Header />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/surveys" component={Dashboard} />
+        <Route path="/surveys/new" component={SurveyNew} />
+      </div>
+      </BrowserRouter>
     </div>
   );
 };
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(reducers,{},applyMiddleware());
 
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+
+
+ReactDOM.render(
+  <Provider store={store}>< App /></Provider>,
+  document.querySelector('#root')
+);
