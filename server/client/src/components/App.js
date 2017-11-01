02f209053662c824bbe4f14883d0bbcfea949845
Hari <hariprasath.periyasamy@gmail.com>
2017-10-31 20:28:18 -0400

    working dispatcher

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+
+
 
 import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
-const App = () => {
-  return (
-    <div>
-      <BrowserRouter>
-      <div>
-        <Header />
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/surveys" component={Dashboard} />
-        <Route path="/surveys/new" component={SurveyNew} />
+
+class App extends Component {
+
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render(){
+    return(
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing}/>
+            <Route exact path="/surveys" component={Dashboard}/>
+            <Route path="/surveys/new" component={SurveyNew}/>
+          </div>
+        </BrowserRouter>
       </div>
-      </BrowserRouter>
-    </div>
-  );
+    );
+  }
 };
 
-export default App;
+
+export default connect(null, actions)(App);
--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,27 +1,23 @@
-import React, { Component } from 'react';
-
-
+import React,{ Component } from 'react';
 
 class Header extends Component {
-  render(){
-    return(
-      <nav>
-      <div className="nav-wrapper">
-        <a className="left brand-logo">
-          WiseCrack
-        </a>
-        <ul className="right">
-          <li>
-            <a>
-              Login
-            </a>
-          </li>
-        </ul>
-      </div>
-        Header
-      </nav>
-    );
-  }
+
+    render() {
+        return (
+          <nav>
+            <div className="nav-wrapper">
+                <a className="left brand-logo">
+                    WiseCrack
+                </a>
+                <ul className="right">
+                  <li>
+                      <a>Login with Google</a>
+                  </li>
+                </ul>
+            </div>
+          </nav>
+        );
+    }
 }
 
 export default Header;
